b4d936099103958337b4e0ed79c0e998bf7cdd43
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-17 19:04:35 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,5 +14,8 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
+    self.songs.collect do |song|
+      song.genre
+    end.uniq.size
   end
 end
